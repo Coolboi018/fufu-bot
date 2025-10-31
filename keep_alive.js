8ed11c16a0047963cde59cf93581ad580cd8e55c
Cool Boi <gamersteve018@gmail.com>
2025-10-31 16:56:17 +0530

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-const app = express();
-app.all("/", (req, res) => res.send("✅ Fufu bot is alive!"));
-app.listen(3000, () => console.log("KeepAlive server started"));
-export default function keep_alive() {}
